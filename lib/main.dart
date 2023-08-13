fe0e7cd48d3f7a1c7be73c0e28a2cd675d704b1c
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2023-08-13 21:40:28 +0500

    changing app font

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
+        fontFamily: 'Poppins',
       ),
       home: const SplashScreen(),
     );
