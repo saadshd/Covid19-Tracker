7107fe7903b72e803800ed90a372cbd7eec5ba17
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2023-08-13 17:38:05 +0500

    creating UI

--- lib/Views/world_stats_screen.dart
+++ lib/Views/world_stats_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WorldStatsScreen extends StatefulWidget {
   const WorldStatsScreen({super.key});
@@ -7,11 +8,108 @@ class WorldStatsScreen extends StatefulWidget {
   State<WorldStatsScreen> createState() => _WorldStatsScreenState();
 }
 
-class _WorldStatsScreenState extends State<WorldStatsScreen> {
+class _WorldStatsScreenState extends State<WorldStatsScreen> with TickerProviderStateMixin{
+
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  final colorList = <Color> [
+    Colors.blue,
+    Colors.green,
+    Colors.red,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      appBar: AppBar(
+        title: const Text('COVID-19 Tracker'),
+      ),
+      body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: MediaQuery.sizeOf(context).height * .01,),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: PieChart(
+                      dataMap: {
+                        // 'Total': double.parse(snapshot.data!.cases.toString()),
+                        'Total': 20,
+                        'Recovered': 15,
+                        'Deaths': 05,
+                      },
+                      chartRadius: MediaQuery.sizeOf(context).height / 5,
+                      legendOptions: const LegendOptions(
+                        legendPosition: LegendPosition.left
+                      ),
+                      animationDuration: const Duration(milliseconds: 1200),
+                      chartType: ChartType.ring,
+                      colorList: colorList,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text('Worldwide Stats',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),),
+                ),
+                Card(
+                  child: Column(
+                    children: [
+                      ReuseableRow(title: 'Total', value: '200'),
+                      ReuseableRow(title: 'Total', value: '200'),
+                      ReuseableRow(title: 'Total', value: '200'),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+
+                  ),
+                    onPressed: (){}, 
+                    child: Text('Track Countries'))
+              ],
+            ),
+          )),
     );
   }
 }
+
+class ReuseableRow extends StatelessWidget {
+  String title, value;
+  ReuseableRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10,10,10,5),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title),
+              Text(value),
+            ],
+          ),
+          const SizedBox(height: 5,),
+          const Divider()
+        ],
+      ),
+    );
+  }
+}
+
