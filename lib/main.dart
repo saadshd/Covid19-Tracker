a1db6441261392fcf9553de910142c2079ce93d4
SAADSHD <101456852+SAADSHD@users.noreply.github.com>
2023-08-13 15:26:39 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:covid19_tracker/Views/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Covid19 Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+
